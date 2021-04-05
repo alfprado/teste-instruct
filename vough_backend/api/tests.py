1a58af8f1d4d8a7959dba76baf250c629420d41a
André Luiz Fernandes do Prado <alfprado@localhost.localdomain>
2021-04-05 18:40:40 -0300

    Adiciona testes da api do git

--- vough_backend/api/tests.py
+++ vough_backend/api/tests.py
@@ -1,3 +1,17 @@
+from api.integrations.github import GithubApi
 from django.test import TestCase
 
-# Create your tests here.
+
+class IntegrationsTests(TestCase):
+
+    def setUp(self):
+        self.git = GithubApi()
+
+    def test_organization_public_members(self):
+        self.assertEqual(self.git.get_organization_public_members('instruct-br'), 9)
+
+    def test_organization(self):
+        org = self.git.get_organization('instruct-br')
+
+        self.assertEqual(org['name'], 'Instruct')
+        self.assertEqual(org['public_repos'], 41)
